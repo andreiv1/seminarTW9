a3d8bbefe51c26549887820b02a590dff9a240ca
andreiv1 <vasilievici.andrei01@gmail.com>
2022-11-22 17:54:12 +0200

    Added latest methods

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -42,21 +42,38 @@ router.route('/addTask').post(async (req, res) => {
     }
 })
 
-router.route('/modifyTask/:id').put((req, res) => {
-    let task = tasks.find(task => task.id == req.params.id)
-
-    task.title = req.body.title;
-    task.isDone = req.body.isDone;
-
-    res.json(task)
+router.route('/modifyTask/:id').put(async (req, res) => {
+    try{
+        const task = await Task.findByPk(req.params.id)
+        if(task){
+            const updatedTask = await task.update(req.body);
+            res.status(200).json(updatedTask);
+        } else {
+            res.status(404).json({"error" : `Task with id ${req.params.id} not found!`});
+        }
+    } catch(err) {
+        res.status(500).json(err);
+    }
 })
 
-router.route('/deleteTask/:id').delete((req, res) => {
-    let index = tasks.findIndex(task =>  task.id == req.params.id)
+router.route('/deleteTask/:id').delete(async (req, res) => {
+    try{
+        Task.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then((rows) => {
+            if(rows === 1){
+                res.status(200).json({"error" : `Task with id ${req.params.id} deleted`});
+            } else {
+                res.status(404).json({"error" : `Task with id ${req.params.id} not found!`});
+            }
+        })
+    }
+    catch(err){
 
-    tasks.splice(index, 1);
-
-    res.json(tasks);
+    }
 })
 
 module.exports = router;
