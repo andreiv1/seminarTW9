5f1830ca6d117a889a82c7fda7e68b81f02f753a
andreiv1 <vasilievici.andrei01@gmail.com>
2022-11-22 17:31:28 +0200

    Added ORM

--- server/Task.js
+++
@@ -1,9 +0,0 @@
-class Task{
-    constructor(id, title, isDone){
-        this.id = id;
-        this.title = title;
-        this.isDone = isDone;
-    }
-}
-
-module.exports = Task;
--- server/index.js
+++ server/index.js
@@ -1,21 +1,31 @@
 let express = require('express')
 let bodyParser = require('body-parser')
 let cors = require('cors')
-const router = require('./routes/tasks.js')
+const router = require('./routes/tasks')
 require('dotenv').config();
 let app = express()
+require('./models/task')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
 app.use('/api', router)
 
+const sequelize = require('./sequelize');
 
 app.use((err,req, res, next) => {
     res.status(500).json({"message": "Internal Server Error"})
 })
 app.set("port",process.env.PORT || 8080)
 
-app.listen(app.get('port'), () => {
-    console.log('Server listening on port ' + app.get('port'))
+app.listen(app.get('port'), async () => {
+    console.log('Server listening on port ' + app.get('port'));
+
+    try{
+        await sequelize.authenticate();
+        console.log('Connected to database!')
+    }
+    catch(err){
+        console.log('Unable to connect to database: ', err);
+    }
 })
